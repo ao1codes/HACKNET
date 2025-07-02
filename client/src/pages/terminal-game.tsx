9562b0186c9da9fd79d29b0665dd9a0fe898191e
ao1codes <aadilamhs@gmail.com>
2025-07-02 09:11:00 -0400

    main game page

---
+++ client/src/pages/terminal-game.tsx
@@ -0,0 +1,9 @@
+import TerminalInterface from '@/components/terminal/terminal-interface';
+
+export default function TerminalGame() {
+  return (
+    <div className="w-full h-screen bg-black overflow-hidden">
+      <TerminalInterface />
+    </div>
+  );
+}
